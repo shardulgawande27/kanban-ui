bf129f821d8d0127002dbc099553c84a32dd3760
shardulgawande27 <shardulgawande27@gmail.com>
2024-08-27 09:24:18 +0530

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
-import Task from "./components/task";
+// import Task from "./components/task";
 import KanbanUi from "./components/KanbanUi";
 
 function App() {
